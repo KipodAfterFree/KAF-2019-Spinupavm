70adf5416cc88b88071fd72b6dd81089644304f0
Nadav Tasger <tashernadav@gmail.com>
2019-11-01 23:26:25 +0200

    Added a flag hint

--- src/main/java/spinupavm/commands/flag.java
+++ src/main/java/spinupavm/commands/flag.java
@@ -3,15 +3,17 @@ package spinupavm.commands;
 import org.quteshell.Command;
 import org.quteshell.Elevation;
 import org.quteshell.Quteshell;
+import org.quteshell.commands.Help;
 
 @Elevation(Elevation.ALL)
+@Help.Description("correct-elevation=>green, else=>red")
 public class flag implements Command {
     @Override
     public void execute(Quteshell shell, String arguments) {
         if (shell.getElevation() < 0) {
-            shell.writeln("KAF{w3ll_p1ay3d_my_hack3r_fr13nd}");
+            shell.writeln("KAF{w3ll_p1ay3d_my_hack3r_fr13nd}", Quteshell.Color.LightGreen);
         } else {
-            shell.writeln("KAF{damn_1_0nly_had_t0_writ3_flag}");
+            shell.writeln("KAF{damn_1_0nly_had_t0_writ3_flag}", Quteshell.Color.LightRed);
         }
     }
 }
